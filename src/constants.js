5f827da937e8319b3b6553e76510521437c7a34e
Anders Hallem Iversen <andershallemiversen@hotmail.com>
2018-07-23 16:48:55 +0200

    Fixed import issue in constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,6 @@
 // Constants used in the module
-module.exports.API = 'https://api.recrut.no/';
+const API = 'https://api.recrut.no/';
+module.exports.API = API;
 module.exports.BASE = API + 'v1/';
 module.exports.FORMAT = '?format=json';
 module.exports.NO_AUTH = 'NO_AUTH';
