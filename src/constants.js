c8c37f655d05da3b0666c1dc4ae06aa633064db9
Anders Hallem Iversen <andershallemiversen@hotmail.com>
2018-08-07 23:25:41 +0200

    Changes base url

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,11 @@
 // Constants used in the module
-export const API = 'https://api.recrut.no/';
+const env = process.env;
+console.log("ENV", env);
+
+const API_URL = env.REACT_APP_API_URL || 'https://api.recrut.no/';
+console.log("API_URL", API_URL);
+
+export const API = API_URL;
 export const BASE = API + 'v1/';
 export const FORMAT = '?format=json';
 export const NO_AUTH = 'NO_AUTH';
